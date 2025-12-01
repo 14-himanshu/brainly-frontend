7318fe0e00c876ed6b7a849c2529bc9f55e99183
Himanshu Pandey <himanshup01428@gmail.com>
2025-12-01 14:06:25 +0530

    created the card section dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,27 @@ import { Shareicon } from "./icons/ShareIcon";
 
 function App() {
   return (
-    <div>
-      <Button variant="primary" text="Add content" startIcon={<PlusIcon/>}/>
-      <Button variant="secondary" text="Share brain" startIcon={<Shareicon/>}/>
-
-      <Card/>
+    <div className="p-4">
+      <div className="flex justify-end gap-4">
+        <Button variant="primary" text="Add content" startIcon={<PlusIcon />} />
+        <Button
+          variant="secondary"
+          text="Share brain"
+          startIcon={<Shareicon />}
+        />
+      </div>
+      <div className="flex gap-4">
+        <Card
+          title="First tweet"
+          link="https://twitter.com/BannedV1ds/status/1994893144710942843?s=20"
+          type="twitter"
+        />
+        <Card
+          title="First video"
+          link="https://youtu.be/x0Edm0w9pc0"
+          type="youtube"
+        />
+      </div>
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,54 +1,58 @@
 import { Shareicon } from "../icons/ShareIcon";
 
-interface CardProps{
-    title: string;
-    link: string;
-    type: "twitter" | "youtube";
+interface CardProps {
+  title: string;
+  link: string;
+  type: "twitter" | "youtube";
 }
 
-
-export function Card({title,link, type}: CardProps){
-    return (
-      <div>
-        <div
-          className="p-4 bg-white rounded-md border-gray-200 
-        max-w-72 border"
-        >
-          <div className="flex justify-between ">
-            <div className="flex items-center text-md">
-              <div className="text-gray-500 pr-2">
-                <Shareicon />
-              </div>
-              {title}
+export function Card({ title, link, type }: CardProps) {
+  return (
+    <div>
+      <div
+        className="p-4 bg-white rounded-md border-gray-200 
+        max-w-72 border min-h-48 min-w-72"
+      >
+        <div className="flex justify-between ">
+          <div className="flex items-center text-md">
+            <div className="text-gray-500 pr-2">
+              <Shareicon />
             </div>
-            <div className="flex items-center">
-              <div className="pr-2 text-gray-500">
-                <a href={link} target="_blank" ></a>
-                    <Shareicon />
-              </div>
+            {title}
+          </div>
+          <div className="flex items-center">
+            <div className="pr-2 text-gray-500">
+              <a href={link} target="_blank"></a>
+              <Shareicon />
+            </div>
 
-              <div className="text-gray-500">
-                <Shareicon />
-              </div>
+            <div className="text-gray-500">
+              <Shareicon />
             </div>
           </div>
-          <div className="pt-4">
-            {type === "youtube" && <iframe
-              className="w-full"
-              src= {link.replace("watch", "embed")}
+        </div>
+        <div className="pt-4">
+          {type === "youtube" && (
+            <iframe
+              className="w-full aspect-video rounded"
+              src={`https://www.youtube.com/embed/${link
+                .split("/")
+                .pop()
+                ?.replace("watch?v=", "")}`}
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
               allowFullScreen
-            ></iframe>}
+            ></iframe>
+          )}
 
-            {type=="twitter"}
-            <blockquote className="twitter-tweet">
-              <a href={link}></a>
-            </blockquote>
-          </div>
+          {type == "twitter"}
+          <blockquote className="twitter-tweet">
+            <a href={link.replace("x.com", "twitter.com")}></a>
+          </blockquote>
         </div>
       </div>
-    );
+    </div>
+  );
 }
