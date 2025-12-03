3cf02211d816e9686e9b6fc11bb031fc85536e44
Himanshu Pandey <himanshup01428@gmail.com>
2025-12-03 15:13:43 +0530

    added the sidebar and the addcontent page

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,50 @@
+import { useState } from "react";
 import { Button } from "./components/Button";
 import { Card } from "./components/Card";
+import { CreateContentModal } from "./components/CreateContentModal";
 import { PlusIcon } from "./icons/PlusIcon";
 import { Shareicon } from "./icons/ShareIcon";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
-    <div className="p-4">
-      <div className="flex justify-end gap-4">
-        <Button variant="primary" text="Add content" startIcon={<PlusIcon />} />
-        <Button
-          variant="secondary"
-          text="Share brain"
-          startIcon={<Shareicon />}
+    <div>
+      <Sidebar />
+      <div className="p-4 ml-72 min-h-screen bg-gray-100 ">
+        <CreateContentModal
+          open={modalOpen}
+          onClose={() => {
+            setModalOpen(false);
+          }}
         />
-      </div>
-      <div className="flex gap-4">
-        <Card
-          title="First tweet"
-          link="https://twitter.com/BannedV1ds/status/1994893144710942843?s=20"
-          type="twitter"
-        />
-        <Card
-          title="First video"
-          link="https://youtu.be/x0Edm0w9pc0"
-          type="youtube"
-        />
+        <div className="flex justify-end gap-4">
+          <Button
+            onClick={() => {
+              setModalOpen(true);
+            }}
+            variant="primary"
+            text="Add content"
+            startIcon={<PlusIcon />}
+          />
+          <Button
+            variant="secondary"
+            text="Share brain"
+            startIcon={<Shareicon />}
+          />
+        </div>
+        <div className="flex gap-4">
+          <Card
+            title="First tweet"
+            link="https://twitter.com/BannedV1ds/status/1994893144710942843?s=20"
+            type="twitter"
+          />
+          <Card
+            title="First video"
+            link="https://youtu.be/x0Edm0w9pc0"
+            type="youtube"
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,6 +4,7 @@ interface ButtonProps{
     variant : "primary" | "secondary",
     text : String,
     startIcon : ReactElement,
+    onClick? : () => void
 }
 const variantClasses = {
     primary : "bg-purple-600 text-white",
@@ -12,10 +13,10 @@ const variantClasses = {
 
 const defaultStyles = "px-4 py-2 rounded-md font-light flex justify-center items-center"
 
-export function Button({variant,text,startIcon}: ButtonProps){
+export function Button({variant,text,startIcon,onClick}: ButtonProps){
 
     return (
-      <button className={variantClasses[variant] + " " + defaultStyles}>
+      <button onClick = {onClick} className={variantClasses[variant] + " " + defaultStyles}>
         <div className="pr-2">
             {startIcon}
         </div>
